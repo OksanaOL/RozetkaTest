d80b2b701888452ba1ac9ad70e596cb62aec63d9
Oksana <oromanovych@gmail.com>
2020-07-16 01:27:29 +0300

    Working with product cart.

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,53 +1,49 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class AppTest {
     WebDriver browser;
+    private String baseURI = System.getenv("baseURI");
 
     @BeforeTest
-    public void preCondition() {
-
-//        System.setProperty("webdriver.chrome.driver", "d:\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.rozetka.com.ua/");
-        browser.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    public void startWebPage() {
+        if (baseURI == null) {
+            baseURI = "https://google.com.ua";
+        }
+        browser = PreConditions.initWebDriver();
+        PreConditions preConditions = new PreConditions(browser);
+        preConditions.openPage(baseURI)
+                .scrollPage();
     }
-
-    @AfterTest
-    public void postCondition() {
-        browser.close();
-    }
-
     @Test
     public void searchGoods() {
-        SearchProducts searchProducts = new SearchProducts(browser);
-        searchProducts.fillInSearchField("Навушники Xiaomi AirDots/Earbuds Basic TWS (TWSEJ04LS)")
-                .pressSearchButton()
-                .pressBuyButton()
-                .closeWindow();
+        CurrencySymbolCheck currencySymbolCheck = new CurrencySymbolCheck(this.browser);
+        currencySymbolCheck.typeToSearchField("Навушники Xiaomi AirDots/Earbuds Basic TWS (TWSEJ04LS)")
+                .clickSearchButton()
+                .clickBuyButton()
+                .openPopup()
+                .increaseAmount()
+                .closePopup();
     }
 
     @Test
-    public void checkAllPricesAreInGrivna() {
-        SearchProducts searchProducts = new SearchProducts(browser);
-        String expectedCurrencySymbol = "₴";
-        Assert.assertTrue(searchProducts.isAllPricesInSpecifiedCurrency(expectedCurrencySymbol));
+    public void checkPrice() {
+        CurrencySymbolCheck currencySymbolCheck = new CurrencySymbolCheck(this.browser);
+        String expectedcurrencySymbol = "₴";
+        Assert.assertTrue(currencySymbolCheck.isPriceCorrect(expectedcurrencySymbol));
     }
 }
+
+
+
+
