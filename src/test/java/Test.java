4c488bc979a5ffaee4fef952547ee0e3fdf698a0
ivan <byteofmydream@gmail.com>
2020-07-23 20:02:44 +0300

    use page objects

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,29 +1,43 @@
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
+import org.example.CartPopup;
+import org.example.PreConditions;
+import org.example.ProductSearch;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
     WebDriver driver;
+    PreConditions preconditions;
+    CartPopup cartPopup;
+    ProductSearch productSearch;
 
     @Given("Open Rozetka home page")
     public void open_rozetka_home_page() {
-        System.setProperty("webdriver.chrome.driver", "d:\\AQA_2020\\chromedriver_win32\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://www.rozetka.com.ua/");
+        preconditions = new PreConditions();
+        driver = preconditions.getDriver();
+        cartPopup = new CartPopup(driver);
+        productSearch = new ProductSearch(driver);
+        preconditions.openPage("https://www.rozetka.com.ua/");
     }
 
     @When("Enter product title in search field")
     public void enter_product_title_in_search_field() {
-        driver.findElement(By.xpath("//*[@name=\"search\"]")).sendKeys("Навушники Xiaomi AirDots/Earbuds Basic");
-
+//        driver.findElement(By.xpath("//*[@name=\"search\"]")).sendKeys("Навушники Xiaomi AirDots/Earbuds Basic");
+    productSearch.typeToSearchField("Навушники Xiaomi AirDots/Earbuds Basic");
     }
 
-    @Then("press Search button")
+    @When("press Search button")
     public void press_search_button() {
-        driver.findElement(By.xpath("//*[@role=\"search\"]/form/button"));
+//        driver.findElement(By.xpath("//*[@role=\"search\"]/form/button"));
+        productSearch.clickSearchButton();
+    }
+
+    @Then("all prices in search result are in grivna")
+    public void check_all_prices_in_hrivna() {
+        String expectedcurrencySymbol = "₴";
+        Assert.assertTrue(productSearch.isPriceCorrect(expectedcurrencySymbol));
     }
 
 }
--- src/test/resources/test.feature
+++ src/test/resources/test.feature
@@ -1,5 +1,9 @@
 Feature: Search product
+
+
+
   Scenario: Search result should show item
     Given Open Rozetka home page
     When Enter product title in search field
-    Then press Search button
+    And press Search button
+    Then all prices in search result are in grivna
