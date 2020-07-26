58fc83c684ae84ccdbcce1d80d15353889fe7f7f
Oksana <oromanovych@gmail.com>
2020-07-26 23:19:55 +0300

    Added scenario outline.

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,29 +1,89 @@
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
+import org.example.CartPopup;
+import org.example.PreConditions;
+import org.example.ProductSearch;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
     WebDriver driver;
+    PreConditions preConditions;
+    ProductSearch productSearch;
+    CartPopup cartPopup;
 
     @Given("Open Rozetka home page")
     public void open_rozetka_home_page() {
-        System.setProperty("webdriver.chrome.driver", "d:\\AQA_2020\\chromedriver_win32\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://www.rozetka.com.ua/");
+        driver = PreConditions.initWebDriver();
+        preConditions = new PreConditions(driver);
+        productSearch = new ProductSearch(driver);
+        cartPopup = new CartPopup(driver);
+        preConditions.openPage("https://www.rozetka.com.ua/");
     }
 
-    @When("Enter product title in search field")
-    public void enter_product_title_in_search_field() {
-        driver.findElement(By.xpath("//*[@name=\"search\"]")).sendKeys("Навушники Xiaomi AirDots/Earbuds Basic");
-
+    @When("Enter product {string} in search field")
+    public void enterProductInSearchField(String title) {
+        productSearch.typeToSearchField(title);
     }
 
-    @Then("press Search button")
+    @Then("Press Search button")
     public void press_search_button() {
-        driver.findElement(By.xpath("//*[@role=\"search\"]/form/button"));
+        productSearch.clickSearchButton();
     }
 
+    @Given("Rozetka site is opened")
+    public void rozetka_site_is_opened() {
+        driver = PreConditions.initWebDriver();
+        preConditions = new PreConditions(driver);
+        productSearch = new ProductSearch(driver);
+        cartPopup = new CartPopup(driver);
+        preConditions.openPage("https://www.rozetka.com.ua/");
+    }
+
+    @When("Scroll till end off page to load all products")
+    public void scroll_till_end_of_page_to_load_all_products() {
+        preConditions.scrollPage();
+    }
+
+    @Then("Prices are shown in Grivna")
+    public void prices_are_shown_in_grivna() {
+        ProductSearch productSearch = new ProductSearch(this.driver);
+        String expectedcurrencySymbol = "₴";
+        Assert.assertTrue(productSearch.isPriceCorrect(expectedcurrencySymbol));
+    }
+
+    @Given("Requested product is shown and buy button is pressed")
+    public void requestedProductIsShownAndBuyButtonIsPressed() {
+        driver = PreConditions.initWebDriver();
+        preConditions = new PreConditions(driver);
+        productSearch = new ProductSearch(driver);
+        cartPopup = new CartPopup(driver);
+        preConditions.openPage("https://www.rozetka.com.ua/");
+        productSearch.typeToSearchField("Навушники Xiaomi AirDots/Earbuds Basic ")
+                .clickSearchButton()
+                .clickBuyButton();
+    }
+
+    @When("Product Cart is opened - item is present")
+    public void productCartIsOpenedItemIsPresent() {
+        cartPopup.openPopup();
+    }
+
+    @And("Plus button is pressed - total price is changed")
+    public void plusButtonIsPressedTotalPriceIsChanged() throws InterruptedException {
+        Thread.sleep(3000);
+        int inititalPrice = cartPopup.totalPrice();
+        cartPopup.increaseAmount();
+        Thread.sleep(3000);
+        int finalPrice = cartPopup.totalPrice();
+        Assert.assertEquals(inititalPrice * cartPopup.PRODUCT_QUANTITY, finalPrice);
+        cartPopup.closePopup();
+    }
+
+    @Then("Product Cart is closed")
+    public void productCartIsClosed() {
+        cartPopup.closePopup();
+    }
 }
--- src/test/resources/test.feature
+++ src/test/resources/test.feature
@@ -1,5 +1,26 @@
 Feature: Search product
-  Scenario: Search result should show item
+  In order to buy some product
+  As private person
+  I want easy find and buy it using online shop
+
+  Scenario Outline: Search result should show item
     Given Open Rozetka home page
-    When Enter product title in search field
-    Then press Search button
+    When Enter product "<title>" in search field
+    Then Press Search button
+    Examples:
+      | title                                  |
+      | Навушники Xiaomi AirDots/Earbuds Basic |
+      | Samsung Galaxy M21 4/64GB Green        |
+
+
+  Scenario: Verification if all prices are shown in grivna
+    Given Rozetka site is opened
+    When Scroll till end off page to load all products
+    Then Prices are shown in Grivna
+
+  Scenario: Verification if product is oin cart after pressing buy button
+    Given Requested product is shown and buy button is pressed
+    When Product Cart is opened - item is present
+    And Plus button is pressed - total price is changed
+    Then Product Cart is closed
+
