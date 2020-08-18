e65a3b3486d2b6aad9a19e0ca08b3d4ced97be19
Oksana <oromanovych@gmail.com>
2020-08-19 00:47:37 +0300

    Parallel execution of tests has been added

---
+++ src/main/java/org/example/PostCondition.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+
+public class PostCondition {
+   private WebDriver driver;
+
+    public PostCondition (WebDriver driver){
+        this.driver=driver;
+    }
+    public void closeBrowser(){
+        driver.close();
+    }
+}
